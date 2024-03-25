f6f370c42d944d7e0e50a75a26fe3cfed87ccffc
jieke <jackylin432@gmail.com>
2024-03-25 16:25:16 +0100

    CREATED CLASS Day_collection TO COLLECT ACTIVITIES FOR MORE DAYS

---
+++ Day_collection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jieke on 25/03/24.
+//
+
+#include "Day_collection.h"
---
+++ Day_collection.h
@@ -0,0 +1,14 @@
+//
+// Created by jieke on 25/03/24.
+//
+
+#ifndef ACTIVITIES_TRACKER_DAY_COLLECTION_H
+#define ACTIVITIES_TRACKER_DAY_COLLECTION_H
+
+
+class Day_collection {
+
+};
+
+
+#endif //ACTIVITIES_TRACKER_DAY_COLLECTION_H
